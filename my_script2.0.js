f70b84d58572f9f57e55ca0eeb699f1ed2e094c2
Simon Lavers <simonlavers3@outlook.com>
2025-07-03 11:13:25 -0300

    operation file complete so it can perform operations and results

--- my_script2.0.js
+++ my_script2.0.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const operatorButtons = document.querySelectorAll(".operator")
 
 const numberButtons = document.querySelectorAll(".number")
@@ -138,3 +139,86 @@ function getOperator() {
 
  
 
+=======
+const operatorButtons = document.querySelector(".operators")
+
+const numberButtons = document.querySelector(".number")
+
+const container = document.querySelector(".container")
+
+const display = document.querySelector(".display")
+
+
+
+
+
+function getNum(arr) {
+
+
+    let num = null
+
+    arr.forEach(button => {
+
+        if (button.id === "one") {
+            num = 1;
+        
+        }
+
+        else if (button.id === "two") {
+            num = 2;
+       
+        }
+
+        else if (button.id === "three") {
+            num = 3;
+       
+        }
+
+        else if (button.id === "four") {
+            num = 4;
+        
+        }
+
+        else if (button.id === "five") {
+            num = 5;
+        
+        }
+
+        else if (button.id === "six") {
+            num = 6;
+        
+        }
+
+        else if (button.id === "seven") {
+            num = 7;
+        
+        }
+
+        else if (button.id === "eight") {
+            num = 8;
+        
+        }
+
+        else if (button.id === "nine") {
+            num = 9;
+        
+        }
+
+        else if (button.id === "zero") {
+            num = 0;
+        
+        }
+
+    });
+
+    return num
+
+
+}
+
+
+
+
+
+
+>>>>>>> 666dcc4 (Made getNum function with forEach())
